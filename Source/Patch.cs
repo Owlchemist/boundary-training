3dd6ffe9c7da419d2fcbcd980c445eeac09124ca
Owlchemist <owlchemist@protonmail.com>
2021-10-24 09:01:36 -0700

    Dryad fix

--- Source/Patch.cs
+++ Source/Patch.cs
@@ -7,26 +7,27 @@ using System;
 
 namespace BoundaryTraining
 {
+	//This is the main training window
 	[HarmonyPatch(typeof(TrainingCardUtility), nameof(TrainingCardUtility.TryDrawTrainableRow))]
 	public class Patch_TryDrawTrainableRow
 	{
 		static public bool Prefix(Pawn pawn, TrainableDef td)
 		{	
-			if (td == ResourceBank.TrainableDefOf.Owl_Boundaries && (pawn.RaceProps.roamMtbDays == null || pawn.RaceProps.Dryad)) return false;
+			if (td == ResourceBank.TrainableDefOf.Owl_Boundaries && (pawn.RaceProps.baseBodySize < 0.6f || pawn.RaceProps.roamMtbDays == null || pawn.RaceProps.Dryad)) return false;
 			return true;
 		}
     }
-
+	//This is the animals tab table
 	[HarmonyPatch(typeof(PawnColumnWorker_Trainable), nameof(PawnColumnWorker_Trainable.DoCell))]
 	public class Patch_DoCell
 	{
 		static public bool Prefix(Pawn pawn, PawnColumnWorker_Trainable __instance)
 		{	
-			if (__instance.def.trainable == ResourceBank.TrainableDefOf.Owl_Boundaries && (pawn.RaceProps.roamMtbDays == null || pawn.RaceProps.Dryad)) return false;
+			if (__instance.def.trainable == ResourceBank.TrainableDefOf.Owl_Boundaries && (pawn.RaceProps.baseBodySize < 0.6f || pawn.RaceProps.roamMtbDays == null || pawn.RaceProps.Dryad)) return false;
 			return true;
 		}
     }
-
+	//Stops roaming event
 	[HarmonyPatch(typeof(MentalStateWorker_Roaming), nameof(MentalStateWorker_Roaming.CanRoamNow))]
 	public class Patch_CanRoamNow
 	{
@@ -35,27 +36,13 @@ namespace BoundaryTraining
 			if (pawn.training.HasLearned(ResourceBank.TrainableDefOf.Owl_Boundaries)) __result = false;
 		}
     }
-
+	//Allows trained animals to use areas
 	[HarmonyPatch(typeof(Pawn_PlayerSettings), nameof(Pawn_PlayerSettings.SupportsAllowedAreas), MethodType.Getter)]
 	public class Patch_SupportsAllowedAreas
 	{
 		static public void Postfix(ref bool __result, Pawn_PlayerSettings __instance)
 		{	
 			if (__instance.pawn.training?.HasLearned(ResourceBank.TrainableDefOf.Owl_Boundaries) ?? false) __result = true;
-		}
-    }
-
-	[HarmonyPatch(typeof(AnimalPenUtility), nameof(AnimalPenUtility.NeedsToBeManagedByRope))]
-	public class Patch_NeedsToBeManagedByRope
-	{
-		static public bool Prefix(ref bool __result, Pawn pawn)
-		{	
-			if (pawn.training?.HasLearned(ResourceBank.TrainableDefOf.Owl_Boundaries) ?? false)
-			{
-				__result = false;
-				return false;
-			}
-			return true;
 		}
     }
 }
